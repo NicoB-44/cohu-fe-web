e7d1e2e66cbc2ab84ceb60ff607115f396a38f8e
akakwel <bakaban@gmail.com>
2025-06-02 22:00:18 +0200

    Fix base app path

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,11 @@ import { RouteObject } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
+const BASE_PATH = "/cohu-fe-web/";
+
 export const routes: RouteObject[] = [
   {
-    path: "/",
+    path: BASE_PATH,
     element: <Home />,
     errorElement: <NotFound />,
   },
